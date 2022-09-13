e18d9ffbd9b7e6c6af9821e17dc83e274e0f94f0
Agudi2 <anishreddygudi@gmail.com>
2022-09-12 22:52:27 -0400

    Update Policy_Anish_Gudi.java

---
+++ Policy_Anish_Gudi.java
@@ -0,0 +1,92 @@
+public void setPolicyNumber(long pNumber)
+    {
+        policyNumber = pNumber;
+    }
+
+
+    public long getPolicyNumber()
+    {
+        return policyNumber;
+    }
+
+
+    public void setFullName(String fullN)
+    {
+        fullName = fullN;
+    }
+
+
+    public String getFullName()
+    {
+        return fullName;
+    }
+
+
+    public void setFirstName(String firstN)
+    {
+        firstName = firstN;
+    }
+
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+
+    public void setLastName(String lastN)
+    {
+        lastName = lastN;
+    }
+
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+
+    public void setAge(int yearsLived)
+    {
+        age = yearsLived;
+    }
+
+
+    public int getAge()
+    {
+        return age;
+    }
+
+
+    public void setSmokingStatus(String smoker)
+    {
+        smokingStatus = smoker;
+    }
+
+
+    public String getSmokingStatus()
+    {
+        return smokingStatus;
+    }
+
+
+    public void setHeight(double heightOfHolder)
+    {
+        height = heightOfHolder;
+    }
+
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public void setWeight(double weightOfHolder)
+    {
+        weight = weightOfHolder;
+    }
+
+    public double getWeight()
+    {
+        return weight;
+    }
